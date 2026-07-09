8ac17e9f8ccfebfee4a9b00f1cdb70500a67e69c
Glugnus <pernet.aurelien@gmail.com>
2026-07-09 14:11:08 +0200

    fix:changement adresse mail contat

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FaFacebook, FaInstagram } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
 
 export default function Footer() {
   return (
